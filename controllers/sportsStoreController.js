34c9d20b4b7e0c2de2443574d28db4cbb98ec034
thu doan <thudoan@thus-MacBook-Pro.local>
2014-12-04 01:18:51 -0600

    first commit

---
+++ controllers/sportsStoreController.js
@@ -0,0 +1,9 @@
+/**
+ * Created by thudoan on 12/3/14.
+ */
+
+sportsStore.controller("SportsStoreController",function(){
+    this.categories=valsFromService.categories;
+    this.products=valsFromService.products;
+
+});
