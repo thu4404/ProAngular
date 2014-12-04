1182ec8123d5c3ed16040872b0520cdd9385466c
thu doan <thudoan@thus-MacBook-Pro.local>
2014-12-04 01:40:56 -0600

    Added content to SportsStore.html

--- controllers/sportsStoreController.js
+++ controllers/sportsStoreController.js
@@ -1,9 +1,14 @@
 /**
  * Created by thudoan on 12/3/14.
  */
+(function(){
+  var sportsStore=angular.module("sportsStore");
+    sportsStore.controller("SportsStoreController",function(){
+        this.categories=valsFromService.categories;
+        this.products=valsFromService.products;
 
-sportsStore.controller("SportsStoreController",function(){
-    this.categories=valsFromService.categories;
-    this.products=valsFromService.products;
+    });
+})();
 
-});
+
+
